1547e810e7734e15a16f49fa3cfc3552c0066916
Amardeep soni <amardeep10as@gmail.com>
2023-07-22 10:07:26 +0545

    updated code for mobile device long press to show custom menu

--- script.js
+++ script.js
@@ -1,59 +1,84 @@
+if (isTouchDevice()) {
+    heading.innerText = 'Long Press to see Awesome Menu!'
+} else {
+    heading.innerText = 'Right Click To see Awesome Menu!'
+}
+let cont = document.getElementById('cont');
+// Desktop right-click
 window.addEventListener('contextmenu', rightFunction);
+
+// Mobile long press
+window.addEventListener('touchstart', touchStart);
+window.addEventListener('touchend', touchEnd);
+
+let timeoutId;
 
 function rightFunction(e) {
     e.preventDefault();
+    showContextMenu(e.clientX, e.clientY);
+}
+function isTouchDevice() {
+    let heading = document.getElementById('heading');
+    return 'ontouchstart' in window || navigator.maxTouchPoints;
+}
+function touchStart(e) {
+    timeoutId = setTimeout(() => {
+        showContextMenu(e.touches[0].clientX, e.touches[0].clientY);
+    }, 500); // Adjust the delay according to your needs (in milliseconds)
+}
 
-    document.body.innerHTML = `<div id="container">
-            <div id="previewCont" class="row">
-                <div class="logo"><img src="icon/preview.png" alt=""></div>
-                <div id="preview">Preview</div>
-            </div>
-            <div id="shareCont" class="row">
-                <div class="logo"><img src="icon/share.png" alt=""></div>
-                <div id="share">Share</div>
-            </div>
-            <div id="getLinkCont" class="row">
-                <div class="logo"><img src="icon/link.png" alt=""></div>
-                <div id="getLink">Get LInk</div>
-            </div>
-            <div id="renameCont" class="row">
-                <div class="logo"><img src="icon/rename.png" alt=""></div>
-                <div id="rename">Rename</div>
-            </div>
-            <div id="deleteCont" class="row">
-                <div class="logo"><img src="icon/delete.png" alt=""></div>
-                <div id="delete">Delete</div>
-            </div>
+function touchEnd() {
+    clearTimeout(timeoutId);
+}
 
-            <div id="settingCont" class="row">
-                <div class="logo"><img src="icon/setting.png" alt=""></div>
-                <div id="setting">Setting</div>
-            </div>
-        </div>`;
+function showContextMenu(x, y) {
+    cont.innerHTML = `<div id="container">
+        <div id="previewCont" class="row">
+            <div class="logo"><img src="icon/preview.png" alt=""></div>
+            <div id="preview">Preview</div>
+        </div>
+        <div id="shareCont" class="row">
+            <div class="logo"><img src="icon/share.png" alt=""></div>
+            <div id="share">Share</div>
+        </div>
+        <div id="getLinkCont" class="row">
+            <div class="logo"><img src="icon/link.png" alt=""></div>
+            <div id="getLink">Get LInk</div>
+        </div>
+        <div id="renameCont" class="row">
+            <div class="logo"><img src="icon/rename.png" alt=""></div>
+            <div id="rename">Rename</div>
+        </div>
+        <div id="deleteCont" class="row">
+            <div class="logo"><img src="icon/delete.png" alt=""></div>
+            <div id="delete">Delete</div>
+        </div>
+
+        <div id="settingCont" class="row">
+            <div class="logo"><img src="icon/setting.png" alt=""></div>
+            <div id="setting">Setting</div>
+        </div>
+    </div>`;
+
     let containerEl = document.getElementById('container');
 
-    // for width 
-    let newWidth = window.innerWidth - e.clientX;
-    // console.log(newWidth);
-    if (newWidth <= 250) {
+    // Positioning the context menu
+    let newWidth = window.innerWidth - x;
+    if (newWidth <= containerEl.offsetWidth) {
         containerEl.style.right = `${newWidth}px`
-    }
-    else {
-        containerEl.style.left = `${e.clientX}px`;
+    } else {
+        containerEl.style.left = `${x}px`;
     }
 
-    // for height
-    let newHeight = window.innerHeight - e.clientY;
-    console.log(newHeight);
-    if (newHeight <= 270) {
+    let newHeight = window.innerHeight - y;
+    if (newHeight <= 250) {
         containerEl.style.bottom = `${newHeight}px`
-    }
-    else {
-        containerEl.style.top = `${e.clientY}px`;
+    } else {
+        containerEl.style.top = `${y}px`;
     }
 }
 
-window.addEventListener('click', leftClicked)
-function leftClicked() {
-    document.body.innerHTML = ''
-}
+// Close the context menu on any click (left click, touch, etc.)
+window.addEventListener('click', function () {
+    cont.innerHTML = '';
+});
